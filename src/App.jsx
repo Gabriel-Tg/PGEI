f2c1caae169d6074acded53f1182d08ae657197a
Gabriel-Tg <gabrielalvesdesiqueira683@gmail.com>
2026-03-10 11:34:13 -0300

    Site 1.0 Oficial

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import SiteApp from './site/SiteApp'
 
 export default function App() {
   const hostname = String(window.location.hostname || '').toLowerCase()
-  const isDemoHost = hostname === 'demo.localhost'
+  const demoHosts = new Set(['demo.localhost', 'demo.techargos.com.br'])
+  const isDemoHost = demoHosts.has(hostname)
 
   return isDemoHost ? <DemoApp /> : <SiteApp />
 }
